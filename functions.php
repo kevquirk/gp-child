930ac141aa2e59ca9235bebfd0d54bafa9b89004
Kev Quirk <kev@quirk.cc>
2021-04-28 16:44:02 +0100

    pt-sans

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@
  * Add your custom PHP in this file.
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
+
+ /* Load custom fonts to the customiser */
+ add_filter( 'generate_typography_default_fonts', function( $fonts ) {
+    $fonts[] = 'PT Sans Narrow';
+
+    return $fonts;
+} );
