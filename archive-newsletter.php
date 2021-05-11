04b9e14eb833ccd32e985dc18138fda8d5482a6b
Kev Quirk <kev@quirk.cc>
2021-05-11 16:00:56 +0100

    ghj

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -32,7 +32,7 @@ get_header(); ?>
 					 * @hooked generate_archive_title - 10
 					 */
            ?>
-					<h1 class="entry-title">Newsletter Archive</h1>
+					<h1 class="archive-title">Newsletter Archive</h1>
 
           <?php
 					while ( have_posts() ) :
