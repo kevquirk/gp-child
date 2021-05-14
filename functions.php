bf5bb4ac0f9a4c1bb462844afb12fcc8299e1f2b
Kev Quirk <kev@quirk.cc>
2021-05-14 18:24:55 +0100

    Removed Archive prefix

--- functions.php
+++ functions.php
@@ -20,3 +20,19 @@ add_editor_style( 'style.css' );
 add_editor_style( 'editor-style.css' );
 
 add_filter( 'pre_get_posts', 'exclude_category_home' );
+
+// Remove "Archive:", "Category:" etc.
+add_filter( 'get_the_archive_title', function ($title) {
+      if ( is_category() ) {
+              $title = single_cat_title( '', false );
+          } elseif ( is_tag() ) {
+              $title = single_tag_title( '', false );    
+          } elseif ( is_author() ) {
+              $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+          } elseif ( is_tax() ) { //for custom post types
+              $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+          } elseif (is_post_type_archive()) {
+              $title = post_type_archive_title( '', false );
+          }
+      return $title;
+  });
