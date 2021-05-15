d2f95b189acae983798bdd7419641f36ab62e89d
Kev Quirk <kev@quirk.cc>
2021-05-15 13:34:04 +0100

    Added notes to feed

--- functions.php
+++ functions.php
@@ -47,3 +47,10 @@ add_filter( 'get_the_archive_title', function ($title) {
 	$title .=  $title_postfix;
 	return $title;
 },50);
+
+// Add Notes to main RSS imap_fetchheaderfunction myfeed_request($qv) {
+    if (isset($qv['feed']) && !isset($qv['post_type']))
+        $qv['post_type'] = array('notes');
+    return $qv;
+}
+add_filter('request', 'myfeed_request');
