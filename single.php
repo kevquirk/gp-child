d69ac7452c2f0115860fe2044f61fe8157fcce68
Kev Quirk <kev@quirk.cc>
2021-07-02 12:09:10 +0100

    Added old post notice

---
+++ single.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+	<div id="primary" <?php generate_do_element_classes( 'content' ); ?>>
+		<main id="main" <?php generate_do_element_classes( 'main' ); ?>>
+			<?php
+			/**
+			 * generate_before_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_before_main_content' );
+
+			if ( generate_has_default_loop() ) {
+
+      // Old post notice
+        $old_post = 60*60*24*365; // This is a year
+        if((date('U')-get_the_time('U')) > $old_post) {
+        echo '<div class="notice"><p>This post was last updated over a year ago, therefore the contents of this post may be out of date. Please see <a href="/disclaimer">my disclaimer</a> for more info.</p></div>';
+        }
+
+				while ( have_posts() ) :
+
+					the_post();
+
+					generate_do_template_part( 'single' );
+
+				endwhile;
+			}
+
+			/**
+			 * generate_after_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_after_main_content' );
+			?>
+		</main>
+	</div>
+
+	<?php
+	/**
+	 * generate_after_primary_content_area hook.
+	 *
+	 * @since 2.0
+	 */
+	do_action( 'generate_after_primary_content_area' );
+
+	generate_construct_sidebars();
+
+	get_footer();
