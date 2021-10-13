41a566b68a2f7efc1bd8a4e8c0e6080c2e5812a2
Kev Quirk <kev@quirk.cc>
2021-10-13 17:50:18 +0100

    Revert jQuery

--- functions.php
+++ functions.php
@@ -76,12 +76,3 @@ add_shortcode( 'mailto_title', 'mailto_title' );
 function mailto_title( $atts ) {
     return esc_attr( get_the_title( get_the_ID() ) );
 }
-
-/** * Completely Remove jQuery From WordPress */
-function my_init() {
-    if (!is_admin()) {
-        wp_deregister_script('jquery');
-        wp_register_script('jquery', false);
-    }
-}
-add_action('init', 'my_init');
